3332de09dd306121ddafced7e910cd363093e372
Nabil Sekirime <nabil@soulcadeinteractive.com>
2019-07-28 23:05:15 -0400

    Challenge Updates

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
+    public List<GameObject> hazards = new List<GameObject>();
+    public int WinningScore;
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawnWait;
@@ -16,10 +19,12 @@ public class GameController : MonoBehaviour {
 	private int score;
 	private bool gameOver;
 	private bool restart;
+    private string gameoverText;
 
 	void Start() {
 		gameOver = false;
 		restart = false;
+        gameoverText = "Game Over";
 		restartText.text = "";
 		gameOverText.text = "";
 		score = 0;
@@ -41,7 +46,7 @@ public class GameController : MonoBehaviour {
 			for (int i = 0; i < hazardCount; ++i) {
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (hazard, spawnPosition, spawnRotation);
+				Instantiate (hazards[Random.Range(0,hazards.Count)], spawnPosition, spawnRotation);
 				yield return new WaitForSeconds(spawnWait);
 			}
 			yield return new WaitForSeconds(waveWait);
@@ -59,10 +64,15 @@ public class GameController : MonoBehaviour {
 
 	void UpdateScore() {
 		scoreText.text = "Score: " + score;
+        if (score >= WinningScore)
+        {
+            gameoverText = "You Win! \n Game created by Nabil Sekirime";
+            GameOver();
+        }
 	}
 
 	public void GameOver() {
 		gameOver = true;
-		gameOverText.text = "Game Over";
+		gameOverText.text = gameoverText;
 	}
 }
